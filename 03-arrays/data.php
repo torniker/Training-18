812715e106a2d156e0681d35f2b939f3ab63e851
Tornike Razmadze <torniker@gmail.com>
2017-01-30 22:07:26 +0400

    added list data

--- 03-arrays/data.php
+++ 03-arrays/data.php
@@ -1,15 +1,21 @@
 <?php
 $nav = [
-    ['id' => 'home', 'name' => 'Home'],
-    ['id' => 'about', 'name' => 'About'],
-    ['id' => 'blog', 'name' => 'Blog'],
-    ['id' => 'contact', 'name' => 'Contact'],
-    ['id' => 'foo', 'name' => 'Foo'],
+    ['id' => 'home', 'name' => 'Home', 'type' => 'text'],
+    ['id' => 'about', 'name' => 'About', 'type' => 'text'],
+    ['id' => 'blog', 'name' => 'Blog', 'type' => 'list'],
+    ['id' => 'contact', 'name' => 'Contact', 'type' => 'text'],
 ];
 $content = [
     'home'    => ['title' => 'Home Page', 'body' => 'home page content'],
     'about'   => ['title' => 'About Us', 'body' => 'About content'],
-    'blog'    => ['title' => 'Blog', 'body' => 'Blog content'],
+    'blog'    => [
+        'title' => 'Blog',
+        'list'  => [
+            ['title' => 'sadf', 'body' => 'asfsd'],
+            ['title' => 'sadf', 'body' => 'asfsd'],
+            ['title' => 'sadf', 'body' => 'asfsd'],
+            ['title' => 'sadf', 'body' => 'asfsd'],
+        ],
+    ],
     'contact' => ['title' => 'Contact', 'body' => 'Contact content'],
-    'foo'     => ['title' => 'Foo title', 'body' => 'asdfsadfas'],
 ];
