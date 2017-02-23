42edede514ba2c134afdf5706f0821b8e17d3f20
Tornike Razmadze <torniker@gmail.com>
2017-02-23 21:28:40 +0400

    updated lesson 09

--- 09-cookies/index.php
+++ 09-cookies/index.php
@@ -1,9 +1,34 @@
 <?php
-// session_start();
-// $_SESSION
-// $_COOKIE
-// $_SERVER
-// echo time();
-setcookie('foo', 'bar', time() + 3600);
-echo $_COOKIE['foo'];
-// print_r($_SERVER['REMOTE_ADDR']);
+include 'helpers.php';
+
+if (!isset($_COOKIE['visited'])) {
+    $views = file_get_contents('views.txt');
+    file_put_contents('views.txt', $views + 1);
+    setcookie('visited', 'namkofia', time() + 60 * 60 * 24);
+}
+$cont = parseCSVFile('content.txt', [1 => 'title', 2 => 'body', 3 => 'type'], 0);
+$nav = parseCSVFile('nav.txt', ['id', 'date', 'name', 'views'], 0);
+if (isset($_GET['page']) && isset($cont[$_GET['page']])) {
+    $navStr = '';
+    foreach ($nav as $item) {
+        if ($item['id'] == $_GET['page']) {
+            $views = $item['views'] = $item['views'] + 1;
+        }
+        $navStr .= $item['id'].','.$item['date'].','.$item['name'].','.$item['views']."\n";
+    }
+    file_put_contents('nav.txt', trim($navStr));
+}
+?>
+<ul>
+    <?php foreach ($nav as $item): ?>
+    <li><a href="index.php?page=<?=$item['id']?>"><?=$item['name']?></a></li>
+    <?php endforeach;?>
+</ul>
+<?php
+if (isset($_GET['page']) && isset($cont[$_GET['page']])) {
+    $key = $_GET['page'];
+    echo '<h1>'.$cont[$key]['title'].'</h1>';
+    echo $cont[$key]['body'].'<br>';
+    echo $views;
+}
+?>
